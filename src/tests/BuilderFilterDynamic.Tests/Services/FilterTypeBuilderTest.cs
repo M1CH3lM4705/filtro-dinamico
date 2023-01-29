25d8069e03449cda4c1571b56f8a01118d9c78a1
Michel Matos <michel.mef@gmail.com>
2023-01-29 19:35:53 -0400

    testes

---
+++ src/tests/BuilderFilterDynamic.Tests/Services/FilterTypeBuilderTest.cs
@@ -0,0 +1,26 @@
+﻿using BuilderFilterDynamic.Services;
+using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace BuilderFilterDynamic.Tests.Services
+{
+    public class FilterTypeBuilderTest
+    {
+        [Fact]
+        public void DevoObterExpressionEhIgual()
+        {
+            var pessoa = new Pessoa()
+            {
+                Nome = "Teste",
+                Idade = 1
+            };
+
+            Expression<Func<Pessoa, bool>> lambdaEhIgual = p => p.Nome == "Teste";
+
+            var expression = new FilterTypeBuilder<Pessoa>(pessoa).EhIgual(nameof(pessoa.Nome)).Construir();
+
+            Assert.Equal(expression.ToString(), lambdaEhIgual.ToString());
+        }
+    }
+}
